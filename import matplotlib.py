41a10c4580ca75ad8f94bd3cb7202e697b3fb352
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2024-01-23 12:46:09 -0500

    Data imported, beginning to graph

--- import matplotlib.py
+++ import matplotlib.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-plantData = open("plants_1.csv", "r")
-print (plantData) 
+openFile = open("plants_1.csv", "r")
+listFile = list(openFile)
+plantData = [i.split(",") for i in listFile[1:]] 
+
+# print (plantData) 
+
+# isolate each variable of the plant data into its own list 
+for plant in plantData: 
+    identifier = [int(entry[0]) for entry in plantData] 
+    sepal_width = [float(entry[1]) for entry in plantData]
+    sepal_length = [float(entry[2]) for entry in plantData] 
+    petal_width = [float(entry[3]) for entry in plantData] 
+    petal_length = [float(entry[4]) for entry in plantData]
+
+# check the data is formatted correctly 
+print (sepal_length) 
 
 # graph the data to detect any initial patterns 
 fig, simple_chart = plt.subplots() 
 simple_chart.plot(plantData) 
+
